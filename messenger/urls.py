11b1491be036e93da451289e3d914e94d39caeff
Mark Volkov <markasch67@gmail.com>
2025-05-30 00:31:28 +0300

    Create forms

--- messenger/urls.py
+++ messenger/urls.py
@@ -8,4 +8,10 @@ app_name = MessengerConfig.name
 
 urlpatterns = [
     path("", views.MainTemplateView.as_view(), name="main"),
+
+    path("client/create/", views.ClientCreateView.as_view(), name="client_create"),
+    path("message/create/", views.MessageCreateView.as_view(), name="message_create"),
+    path("mailing/create/", views.MailingCreateView.as_view(), name="mailing_create"),
+
+
 ]
